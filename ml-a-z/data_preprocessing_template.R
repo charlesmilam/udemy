78bc60d8441931340192e7db0a7facfbef7860fa
Charles Milam <charles.milam@gmail.com>
2017-01-19 21:33:58 -0600

    take care of missing values in R

--- ml-a-z/data_preprocessing_template.R
+++ ml-a-z/data_preprocessing_template.R
@@ -3,3 +3,8 @@
 # import the dataset
 dataset = read.csv('Data.csv')
 
+# take care of missing values
+dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
