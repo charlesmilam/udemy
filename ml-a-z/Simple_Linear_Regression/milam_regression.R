e8876490e8c0b9f1831e9b71fec4d4d9e64b97fa
Charles Milam <charles.milam@gmail.com>
2017-01-24 22:11:09 -0600

    begin linear regression on R. import data and split into training and test sets.

---
+++ ml-a-z/Simple_Linear_Regression/milam_regression.R
@@ -0,0 +1,14 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 0.67)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
