680e885792e55bea8917a76de791989344bbf63d
Charles Milam <charles.milam@gmail.com>
2017-01-28 16:19:29 -0600

    fit the linear regression model to the training set.

--- ml-a-z/Multiple_Linear_Regression/milam_multi_regr.py
+++ ml-a-z/Multiple_Linear_Regression/milam_multi_regr.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # suppress printing of scientific notation
 np.set_printoptions(suppress=True)
@@ -29,10 +30,6 @@ X = hotlabler.fit_transform(X).toarray()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # %%
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+# fit multiple linear regression to training set
+regr = LinearRegression()
+regr.fit(X_train, y_train)
