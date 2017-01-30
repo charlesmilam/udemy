ae0c71d377b82fdfd163908e8bd50b2b10fd7d05
Charles Milam <charles.milam@gmail.com>
2017-01-29 21:02:52 -0600

    import data into R and perform preprocesing on that data

---
+++ ml-a-z/Multiple_Linear_Regression/milam_multi_regr.R
@@ -0,0 +1,18 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+# encode the categorical state data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
