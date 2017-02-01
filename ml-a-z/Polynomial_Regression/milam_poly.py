c3c7513ec65b31565460717698238360da6bf7c9
Charles Milam <charles.milam@gmail.com>
2017-01-31 23:22:54 -0600

    create polynomial regression, and fit it.

--- ml-a-z/Polynomial_Regression/milam_poly.py
+++ ml-a-z/Polynomial_Regression/milam_poly.py
@@ -4,8 +4,9 @@
 import numpy as np
 import matplotlib.pyplot as pyplot
 import pandas as pd
-from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler, PolynomialFeatures
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 from pprint import pprint
 
 # suppress printing of scientific notation
@@ -20,3 +21,18 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 # create dependent variable vector
 y = dataset.iloc[:, -1].values
+
+# %%
+# fit linear regression to the dataset
+linear_regr = LinearRegression()
+linear_regr.fit(X, y)
+
+# %%
+# fit polynomial regression to the dataset
+poly_regr = PolynomialFeatures(2)
+X_poly = poly_regr.fit_transform(X)
+
+# %%
+# fit X_poly to new linear regression
+linear_regr2 = LinearRegression()
+linear_regr2.fit(X_poly, y)
