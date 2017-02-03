43ab63b6d86c91dd2b2fe58893c11e8a2036c87a
Charles Milam <charles.milam@gmail.com>
2017-02-02 21:59:13 -0600

    visualize results for python.

--- ml-a-z/Polynomial_Regression/milam_poly.py
+++ ml-a-z/Polynomial_Regression/milam_poly.py
@@ -2,12 +2,15 @@
 # %%
 # import the libraries
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler, PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from pprint import pprint
+
+%matplotlib inline
 
 # suppress printing of scientific notation
 np.set_printoptions(suppress=True)
@@ -29,10 +32,22 @@ linear_regr.fit(X, y)
 
 # %%
 # fit polynomial regression to the dataset
-poly_regr = PolynomialFeatures(2)
+poly_regr = PolynomialFeatures(4)
 X_poly = poly_regr.fit_transform(X)
 
 # %%
 # fit X_poly to new linear regression
 linear_regr2 = LinearRegression()
 linear_regr2.fit(X_poly, y)
+
+# %%
+# visualize the results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X, linear_regr.predict(X), color = 'blue')
+plt.plot(X_grid, linear_regr2.predict(poly_regr.fit_transform(X_grid)), color = 'steelblue')
+plt.title('Truth vs Bluff')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
