35b3b6935a6e76c3d8771a3aa6d6d0e506519b3b
Charles Milam <charles.milam@gmail.com>
2017-02-13 22:00:54 -0600

    add basic state check to check for wins. add basic loop for game play.

--- Complete-Python-Bootcamp-master/milestone_1_milam.py
+++ Complete-Python-Bootcamp-master/milestone_1_milam.py
@@ -7,6 +7,9 @@
 board = [['-']*3 for x in xrange(3)]
 board
 
+# player one?
+is_player_one = True
+
 #%%
 # print the board
 def print_board(board):
@@ -25,13 +28,49 @@ print 'your input:', get_input()
 
 #%%
 # place markers on board
-def place_marker(pos):
+def place_marker(pos, player_one):
     x = int(pos[0])
     y = int(pos[1])
 
-    board[x][y] = 'X'
+    if player_one:
+        board[x][y] = 'X'
+        player_one = False
+    else:
+        board[x][y] = 'O'
+        player_one = True
+
+
+#%%
+# check the game state
+def check_state():
+    if all(p == 'X' for p in board[0]):
+        print 'X wins!'
+        return True
+    elif all(p == 'X' for p in board[1]):
+        print 'X wins!'
+        return True
+    elif all(p == 'X' for p in board[2]):
+        print 'X wins!'
+        return True
+    if all(p == 'O' for p in board[0]):
+        print 'O wins!'
+        return True
+    elif all(p == 'O' for p in board[1]):
+        print 'O wins!'
+        return True
+    elif all(p == 'O' for p in board[2]):
+        print 'O wins!'
+        return True
 
 #%%
 # trial run
-place_marker(get_input())
-print_board(board)
+while not game_over:
+    place_marker(get_input(), is_player_one)
+    if is_player_one:
+        is_player_one = False
+    else:
+        is_player_one = True
+    print_board(board)
+    game_over = check_state()
+    is_player_one
+    
