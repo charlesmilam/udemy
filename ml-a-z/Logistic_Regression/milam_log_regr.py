4bdde5de1ad2de8a3e516927c0d7860d2a93fd8f
Charles Milam <charles.milam@gmail.com>
2017-02-26 17:15:28 -0600

    complete data preprocessing

--- ml-a-z/Logistic_Regression/milam_log_regr.py
+++ ml-a-z/Logistic_Regression/milam_log_regr.py
@@ -13,11 +13,11 @@ np.set_printoptions(suppress=True)
 
 # %%
 # import the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('Social_Network_Ads.csv')
 
 # %%
 # create features matrix
-X = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, 2:-1].values
 # create dependent variable vector
 y = dataset.iloc[:, -1].values
 
@@ -38,10 +38,10 @@ y = dataset.iloc[:, -1].values
 
 # %%
 # splitting the dataset into a training and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # %%
 # feature scaling
-# sc_X = StandardScaler()
-# X_train = sc_X.fit_transform(X_train)
-# X_test = sc_X.transform(X_test)
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
