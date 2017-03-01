520f61381b41b34ecabefc082330499bbaccf103
Charles Milam <charles.milam@gmail.com>
2017-02-28 20:38:36 -0600

    initial commit from provided starter files

---
+++ ml-a-z/K_Nearest_Neighbors/milam_knn.py
@@ -0,0 +1,103 @@
+# Classification template
+#%%
+# import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import seaborn as sns
+import pandas as pd
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+# suppress printing of scientific notation
+np.set_printoptions(suppress=True)
+# inline plots
+%matplotlib inline
+
+#%%
+# import the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, -1]].values
+y = dataset.iloc[:, -1].values
+
+# # %%
+# # take care of missing data
+# imputer = Imputer('NaN')
+# imputer = imputer.fit(X[:, 1:3])
+# X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# # %%
+# # encode categorical data
+# labeler_X = LabelEncoder()
+# X[:, 0] = labeler_X.fit_transform(X[:, 0])
+# hotlabler = OneHotEncoder(categorical_features = [0])
+# X = hotlabler.fit_transform(X).toarray()
+# labeler_y = LabelEncoder()
+# y = labeler_X.fit_transform(y)
+
+#%%
+# split the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+#%%
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# ================ end preprocessing ==========================
+#%%
+# fit classifier to the Training set
+# Create your classifier here
+
+#%%
+# predict the Test set results
+y_pred = classifier.predict(X_test)
+
+#%%
+# make the Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#%%
+# visualize the Training set results
+X_set, y_set = X_train, y_train
+# layout the grid
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+# colorize prediction points
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# set x and y limits
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+# plot the points
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+# boilerplate
+plt.title('Classifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#%%
+# visualize the Test set results
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
