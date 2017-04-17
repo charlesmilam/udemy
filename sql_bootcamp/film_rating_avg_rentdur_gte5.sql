7a2ce0724e35b24ff38424c50163acf7f2e46b01
Charles Milam <charles.milam@gmail.com>
2017-04-16 20:59:05 -0500

    add queries to complete last portion of lecture 6, having clause

---
+++ sql_bootcamp/film_rating_avg_rentdur_gte5.sql
@@ -0,0 +1,5 @@
+﻿select rating, avg(rental_duration)
+from film
+group by rating
+having avg(rental_duration) > 5
+order by rating;
---
+++ sql_bootcamp/payment_cust_gte40_rentals.sql
@@ -0,0 +1,5 @@
+﻿select customer_id, count(rental_id)
+from payment
+group by customer_id
+having count(rental_id) >= 40
+order by customer_id;
