7e4f5d2085304fe1c3b951c53ffd44a1e2329638
Charles Milam <charles.milam@gmail.com>
2017-09-24 22:25:44 -0500

    add numpy arrays to create 0s, 1s and 5s

---
+++ pyml_bootcamp/numpy_exers.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# create an array of 10 zeros
+arr_0s = np.zeros(10)
+print(arr_0s)
+
+# create an array of 10 ones
+arr_1s = np.ones(10)
+print(arr_1s)
+
+# create an array of 10 fives
+arr_5s = np.ones(10) * 5
+print(arr_5s)
