c3b8c7841ee4a9bf5bf5468b85834b5910716471
Charles Milam <charles.milam@gmail.com>
2017-10-02 19:52:31 -0500

    add solution for 2013 single job

--- pyml_bootcamp/sfsal_exers.py
+++ pyml_bootcamp/sfsal_exers.py
@@ -1,5 +1,8 @@
 # import pandas
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # read Salaries.csv as a dataframe called sal
 csv_file = 'Python-Data-Science-and-Machine-Learning-Bootcamp/Python-for-Data-Analysis/Pandas/Pandas Exercises/Salaries.csv'
@@ -29,14 +32,15 @@ print()
 print('person with greatest total benefits: ', sal_df[sal_df['TotalPayBenefits'] == sal_df['TotalPayBenefits'].max()][['EmployeeName', 'TotalPayBenefits']])
 print()
 # alt highest paid person
-print(sal_df.groupby(['EmployeeName'])['TotalPayBenefits'].max())
+print(sal_df.loc[sal_df['TotalPayBenefits'].idxmax()])
 print()
 
 # lowest paid person
 print('person with least total benefits: ', sal_df[sal_df['TotalPayBenefits'] == sal_df['TotalPayBenefits'].min()][['EmployeeName', 'TotalPayBenefits']])
 print()
 # alt lowest paid
-# stuff goes here
+print(sal_df.loc[sal_df['TotalPayBenefits'].idxmin()])
+print()
 
 # average base pay by year
 print('avg base pay by year: ', sal_df.groupby('Year')['BasePay'].mean())
@@ -51,13 +55,14 @@ print('top 5 common jobs:\n', sal_df['JobTitle'].value_counts().head())
 print()
 
 # job titles wih only 1 person in 2013
-# print('2013 single person job titles:', sal_df[(sal_df[sal_df['Year'] == 2013]['JobTitle']) & (sal_df[sal_df['JobTitle'].value_counts() == 1])])
+print('2013 single person job titles:', sum(sal_df[sal_df['Year'] == 2013]['JobTitle'].value_counts() == 1))
 print()
 
 # how many job titles with chief in title
 sal_df['has_chief'] = sal_df['JobTitle'].apply(lambda x: 'chief' in x.lower())
-print(sal_df[sal_df['has_chief'] == True]['JobTitle'].to_string())
+# print(sal_df[sal_df['has_chief'] == True]['JobTitle'].to_string())
 print('number of jobs with Chief in title: ', len(sal_df[sal_df['has_chief'] == True]))
+print()
 
 # bonus => correlation between job title len and TotalPayBenefits
 # add column of job title lengths
@@ -66,8 +71,13 @@ sal_df['title_len'] = sal_df['JobTitle'].apply(lambda x: len(x))
 titlelen_totalcomp_corr = sal_df['title_len'].corr(sal_df['TotalPayBenefits'])
 print('Correlation between Job Title Length and Total Pay Benefits')
 print(titlelen_totalcomp_corr)
-# apply correlation to entire dataframe
 print()
 print('Table - Correlation between Job Title Length and Total Pay Benefits')
 print(sal_df[['title_len', 'TotalPayBenefits']].corr())
+print()
+# apply correlation to entire dataframe and plot it
+# f, ax = plt.subplots(figsize=(10, 8))
+sal_corr = sal_df[['BasePay', 'OvertimePay', 'OtherPay', 'Benefits', 'TotalPay', 'TotalPayBenefits', 'Year', 'has_chief', 'title_len']].corr()
+sal_corr_heat = sns.heatmap(sal_corr, mask=np.zeros_like(sal_corr, dtype=np.bool), cmap=sns.diverging_palette(220, 10, as_cmap=True), square=True)
+plt.show()
 print()
